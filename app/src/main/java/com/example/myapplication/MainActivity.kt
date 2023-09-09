2d310145cf3f0b7c39b962c27f02114a01a65ee8
avbelovolov <avbelovolov@gmail.com>
2023-09-09 16:19:34 +0500

    Layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
